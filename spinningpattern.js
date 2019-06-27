200810fe37cbc49e5eb11f551cf497e0a4b9bffa
Harrisom Groome <harrisom.groome@ad.infosys.com>
2019-06-27 14:58:02 +0100

    added optional colours

--- spinningpattern.js
+++ spinningpattern.js
@@ -2,7 +2,8 @@ let x_vals = [];
 let y_vals = [];
 let angleChange = [];
 let currentAngle = []; //angles stored in degrees
-
+let colourArray = [];
+let counter = 0;
 function setup() {
   createCanvas(window.innerHeight, window.innerHeight - 100);
   background(0);
@@ -10,9 +11,12 @@ function setup() {
   y_vals.push(0);
 
   x_vals.push(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9);
-  angleChange.push(1,2,3,1,2,3,1,2,3);//ensure same number of elements in x_vals and angleChange
+  angleChange.push(1,5,1,1,5,1,1,5,1);//ensure same number of elements in x_vals and angleChange
   populateYs();
   calculateInitialAngle();
+
+  colourArray.push([255,0,0],[0,255,0]);
+  stroke(255);
 }
 
 function updateCurrentAngles() {
@@ -30,21 +34,20 @@ function updatePoints(){
 }
 
 function draw() {
-  background(0);//comment out if you dont want it to reset
-  stroke(255);
+  //background(0);//comment out if you dont want it to reset
+  //stroke(colourArray[counter%colourArray.length]);//comment out if you want white
   updateCurrentAngles();
   updatePoints();
-
     for (let i = 1; i < x_vals.length; i++) {
       let x1 = mapX(x_vals[i - 1]);
       let y1 = mapY(y_vals[i - 1]);
       let x2 = mapX(x_vals[i]);
       let y2 = mapY(y_vals[i]);
-      strokeWeight(1);
+      strokeWeight(1);//thickness of lines
       line(x1, y1, x2, y2);
-      strokeWeight(8);
+      strokeWeight(5);//thickness of points
       point(x1, y1);
     }
   point(mapX(x_vals[x_vals.length - 1]), mapY(y_vals[y_vals.length - 1]));
-
+  counter++;
 }
